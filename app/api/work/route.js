c2e3c0c98631764d6c7e3ffeb7abcde9ae7564a3
Sukrit <sukrit3007@outlook.com>
2024-02-05 23:08:56 +0530

    publish 4

--- app/api/work/route.js
+++ app/api/work/route.js
@@ -1,17 +1,25 @@
 // /src/api/work/route.js
 import { NextResponse } from 'next/server';
-import WorkModel from '@/lib/models/WorkModel'; // Adjust the path as needed
+import WorkModel from '@/lib/models/WorkModel';
 import { ConnectDB } from '@/lib/config/database';
 
-const LoadDb = async()=>{
-  await  ConnectDB();
-  console.log('Connected to the database');
-}
-
-LoadDb();
-
-export const GET = async (request) => {
+// Wrap the database connection in an async function
+const connectToDatabase = async () => {
   try {
+    await ConnectDB();
+    console.log('Connected to the database');
+  } catch (error) {
+    console.error('Error connecting to the database:', error);
+  }
+};
+
+// Only connect to the database when this specific route is hit
+export const GET = async (request) => {
+  // Connect to the database before handling the request
+  await connectToDatabase();
+
+  try {
+    // Fetch data from the database
     const works = await WorkModel.find({});
     return NextResponse.json(works);
   } catch (error) {
