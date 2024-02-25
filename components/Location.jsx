f0fd1e2678c543b8fbe5f1afc44c35d1cdad44ed
Sukrit <sukrit3007@outlook.com>
2024-02-25 14:56:43 +0530

    added map and changed the font style

--- components/Location.jsx
+++ components/Location.jsx
@@ -1,3 +1,4 @@
+'use client'
 import React from 'react'
 import {
     Card,
@@ -7,8 +8,27 @@ import {
     CardHeader,
     CardTitle,
   } from "@/components/ui/card"  
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 
 const Location = () => {
+  const containerStyle = {
+    width: 'full',
+    height: '200px'
+  };
+  
+  const center = {
+    lat: 12.9716,
+    lng: 77.5946
+  };
+  const mapOptions = {
+    zoomControl: false, // Disable zoom control
+    scrollwheel: false, // Disable scroll wheel zoom
+    disableDoubleClickZoom: true, // Disable double click zoom
+    draggable: false, // Disable map dragging
+    disableDefaultUI: true // Disable default UI controls (including zoom)
+  };
+  
+  
   return (
     <Card className="w-full mb-4 bg-inherit border-0">
       <CardHeader className="flex gap-3">
@@ -16,12 +36,21 @@ const Location = () => {
           <p className="text-small text-default-500">location</p>
         </div>
       </CardHeader>
-
       <CardContent>
-        <p>Make beautiful websites regardless of your design experience.</p>
+        <LoadScript googleMapsApiKey="AIzaSyCnIT342iea9yoObPzyGk0LSVGxUaE-UQs" >
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={center}
+          zoom={10}
+          options={mapOptions}
+        >
+          <Marker position={center} />
+        </GoogleMap>
+      </LoadScript>
       </CardContent>
     </Card>
   )
 }
 
 export default Location
+
