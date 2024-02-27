b4b988f8d34c869fd33c5ea725a303b14bc1cb6b
Sukrit <sukrit3007@outlook.com>
2024-02-27 12:21:50 +0530

    radius of spotify fixed

--- components/SpotifyPlayer.jsx
+++ components/SpotifyPlayer.jsx
@@ -5,7 +5,7 @@ const SpotifyPlayer = () => {
     return (
         <div className="relative w-full h-[352px]">
                    <iframe
-                    className="absolute inset-0 w-full h-full rounded-xl"
+                    className="absolute inset-0 w-full h-full rounded-2xl"
                     src="https://open.spotify.com/embed/album/18NOKLkZETa4sWwLMIm0UZ?view=coverart"
                     style={{ border: '0' }}
                     allowFullScreen
