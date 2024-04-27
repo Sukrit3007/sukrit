d4f4c147c8b616df06c0b2e27a4b7195a709442c
Sukrit <sukrit3007@outlook.com>
2024-04-27 16:04:31 +0530

    added globe in more-card.tsx

--- components/cards/more-card.tsx
+++ components/cards/more-card.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import {Card, CardBody} from "@nextui-org/card";
 import { title } from '../primitives';
+import Globe from '../magicui/globe';
 
 const MoreCard = () => {
   return (
     <Card className='bg-lime-300 text-black p-6'>
-      <CardBody>
+      <div className='z-10'>
         <p className={title()}>Work in progress...</p>
-      </CardBody>
+      </div>
+      <Globe className="top-28" />
     </Card>
   )
 }
