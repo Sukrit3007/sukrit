9497404fa58e4ca1af93c06dad4d89755f9ded15
Sukrit <sukrit3007@outlook.com>
2024-04-30 01:40:44 +0530

    provider error fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({
 					fontSans.variable
 				)}
 			>
-				<Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
+				<Providers themeProps={{ attribute: "class", defaultTheme: "dark",children }}>
 					<Craft.Section>
 						<Craft.Container>
 							<div className="flex flex-col h-screen">
