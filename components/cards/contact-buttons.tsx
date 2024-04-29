8008da776b49d62555242749e1748e101286466d
Sukrit <sukrit3007@outlook.com>
2024-04-30 01:38:09 +0530

    fixed give heart button

--- components/cards/contact-buttons.tsx
+++ components/cards/contact-buttons.tsx
@@ -1,39 +1,19 @@
-import { aboutinfo } from '@/data/about'
+
 import { Button } from '@nextui-org/button'
 import { Link } from '@nextui-org/link'
-import { GithubIcon, LinkedinIcon, Mail, WorkflowIcon } from 'lucide-react'
-import React from 'react'
-import { HeartFilledIcon, X} from '../icons'
-import { confetti } from "@tsparticles/confetti";
+import { GithubIcon, LinkedinIcon, WorkflowIcon } from 'lucide-react'
+import React, { useState } from 'react'
+import { HeartFilledIcon, HeartIcon, X} from '../icons'
+import { aboutinfo } from '../../data/about'
 
 
 
 const ContactButtons = () => {
-    const handleClick = (event:any) => {
-        const buttonRect = event.target.getBoundingClientRect(); // Get button's bounding rectangle
-        const centerX = buttonRect.left + buttonRect.width / 2; // Calculate button's center X coordinate
-        const centerY = buttonRect.top + buttonRect.height / 2; // Calculate button's center Y coordinate
-    
-        confetti("tsparticles", {
-            count: 40,
-            spread: 35,
-            startVelocity: 15,
-            decay: 0.9,
-            gravity: 1,
-            drift: 0,
-            ticks: 200,
-            colors: ["#ffffff", "#ff0000"],
-            shapes: ["square", "circle"],
-            scalar: 1,
-            zIndex: 100,
-            disableForReducedMotion: true,
-            position: {
-                x: (centerX / window.innerWidth) * 100,
-                y: (centerY / window.innerHeight) * 100
-            }
-        });
+    const [heartFilled, setHeartFilled] = useState(false);
+
+    const handleHeartClick = () => {
+        setHeartFilled(!heartFilled);
     };
-    
 
     return (
 
@@ -85,10 +65,10 @@ const ContactButtons = () => {
                     as={Link}
                     color="danger"
                     variant="flat"
-                    startContent={<HeartFilledIcon/>}
-                    onClick={handleClick} 
+                    onClick={handleHeartClick}
+                    startContent={heartFilled ? <HeartFilledIcon /> : <HeartIcon />}
                 >
-                    Give me heart
+                     {heartFilled ? 'You gave heart' : 'Give me heart'}
                 </Button>
             </div>
         </div>
--- components/icons.tsx
+++ components/icons.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
-import { IconSvgProps } from "@/types";
+import { IconSvgProps } from "../types";
+
 
 export const Logo: React.FC<IconSvgProps> = ({
   size = 36,
@@ -107,6 +108,38 @@ export const SunFilledIcon = ({
     </g>
   </svg>
 );
+
+export const HeartIcon = ({
+  size = 24,
+  height,
+  width,
+  ...props
+}: {
+  size?: number;
+  height?: number;
+  width?: number;
+  [key: string]: any; // Allow any other props
+}) => {
+  return (
+    <svg
+      width={size || width || 24}
+      height={size || height}
+      viewBox="0 0 24 24"
+      fill='none'
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M12.62 20.81c-.34.12-.9.12-1.24 0C8.48 19.82 2 15.69 2 8.69 2 5.6 4.49 3.1 7.56 3.1c1.82 0 3.43.88 4.44 2.24a5.53 5.53 0 0 1 4.44-2.24C19.51 3.1 22 5.6 22 8.69c0 7-6.48 11.13-9.38 12.12Z"
+        stroke='currentColor'
+        strokeWidth={1.5}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
 
 export const HeartFilledIcon = ({
   size = 24,
